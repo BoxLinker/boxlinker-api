2b87a190c56951d1c0e0ea43076e7d2b445c2bbd
cabernety <330785652@qq.com>
2017-10-11 23:47:32 +0800

    123

--- pageconfig.go
+++ pageconfig.go
@@ -15,8 +15,8 @@ func (pc PageConfig) Offset() int {
 
 func (pc PageConfig) PaginationJSON() map[string]int {
 	m := map[string]int{}
-	m["current_page"] = pc.CurrentPage
-	m["page_count"] = pc.PageCount
-	m["total_count"] = pc.TotalCount
+	m["currentPage"] = pc.CurrentPage
+	m["pageCount"] = pc.PageCount
+	m["totalCount"] = pc.TotalCount
 	return m
 }
